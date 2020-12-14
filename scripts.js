45c4e27085a1a0053f41913446ac067abf207755
DannyBoyNYC <daniel.deverell@gmail.com>
2020-12-14 14:50:43 -0600

    corrected animation

--- scripts.js
+++ scripts.js
@@ -2,20 +2,16 @@
 const animate = (imgArr) => {
   const arrLen = imgArr.length;
   let activeImgIndex = 0;
-
   setInterval(function () {
     var activeImg = document.querySelector("header img.active");
-    // console.log(" indexOf(activeImg) ", imgArr.indexOf(activeImg));
     if (imgArr.indexOf(activeImg) === arrLen - 1) {
-      console.log(" 1 ", imgArr.indexOf(activeImg), arrLen - 1);
       activeImgIndex = 0;
     } else {
-      console.log(" 2 ", activeImgIndex);
       activeImgIndex++;
-      activeImg.classList.remove("active");
-      imgArr[activeImgIndex].classList.add("active");
     }
-  }, 5000);
+    activeImg.classList.remove("active");
+    imgArr[activeImgIndex].classList.add("active");
+  }, 3000);
 };
 
 const header = document.querySelector("header");
