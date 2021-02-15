f01eaca4f41af62068d3eb1ad9773f9de84f1579
DannyBoyNYC <daniel.deverell@gmail.com>
2021-02-15 16:56:51 -0500

    updating entries

--- js/ma-features.js
+++ js/ma-features.js
@@ -1,15 +1,11 @@
-// export
 const maFeatures = {
   type: "FeatureCollection",
   features: [
     {
       type: "Feature",
       properties: {
-        entry_id: 101,
         url: "http://villatekla.com",
         name: "Villa Tekla",
-        modified_date: "2021/01/01",
-        created_date: null,
         short_description: "A rainforest retreat on 5 hectares.",
         long_description:
           "Available on AirBnB and VRBO, Villa Tekla is a three bedroom Spanish style villa with a private pool.",
@@ -23,11 +19,8 @@ const maFeatures = {
     {
       type: "Feature",
       properties: {
-        entry_id: 100,
         url: "",
         name: "Quepos",
-        modified_date: "2021/01/01",
-        created_date: "2021/01/01",
         short_description:
           "The town of Quepos is a former banana shipping port.",
         long_description:
@@ -43,11 +36,8 @@ const maFeatures = {
     {
       type: "Feature",
       properties: {
-        entry_id: 102,
         url: "",
         name: "Barba Roja",
-        modified_date: "2021/01/03",
-        created_date: null,
         short_description: "The oldest bar and restaurant in Manuel Antonio",
         long_description:
           "If there is a local bar in Manuel Antonio - Barba Roja is it. The food is eclipsed by the happy hour specials but the real star here is the sunset view - the best in the area.",
@@ -61,11 +51,8 @@ const maFeatures = {
     {
       type: "Feature",
       properties: {
-        entry_id: 103,
         url: "",
         name: "Biesanz Beach",
-        modified_date: "2021/01/03",
-        created_date: null,
         short_description: "A quiet beach suitable for children.",
         long_description:
           "A shady beach on the Biesanz penninsula. The snorkeling is decent and equipment can usually be rented in the high season. Park near the entrance, pay the caretaker $2 and walk down the short trail.",
@@ -79,29 +66,23 @@ const maFeatures = {
     {
       type: "Feature",
       properties: {
-        entry_id: 104,
-        url: "http://marinapezvela.com",
-        name: "Marina Pez Vela",
-        modified_date: "2021/01/03",
-        created_date: null,
-        short_description: "American style ammenities in a resort atmosphere.",
+        url: "",
+        name: "Parque Nahomi",
+        short_description: "A local favorite a short walk from the marina",
         long_description:
-          "Fishing trips, a couple decent restaurants and bars, shopping, and cruises. When your looking for Miami-style outings. Pricey",
-        picture: "pezvella.jpg",
+          'A small "urban" park where the Quepoans go to watch the sunset while the children play. Just past the glitzy (for Quepos) Marina Pez Vela and near Teatro Copaza - the sole live theater venue in the area.',
+        picture: "nahomi.jpg",
       },
       geometry: {
         type: "Point",
-        coordinates: [-84.16747, 9.42628],
+        coordinates: [-84.16935, 9.42235],
       },
     },
     {
       type: "Feature",
       properties: {
-        entry_id: 105,
         url: "https://goo.gl/maps/isLGVBBVxaLb1P8y7",
         name: "Playitas",
-        modified_date: "2021/01/03",
-        created_date: null,
         short_description: "The prefered portion of the main beach",
         long_description:
           "Less, crowded, less commercial - this spot is a favorite with surfers and locals in the know. Swim here and then head further down the beach for a snack and a bus/taxi home.",
@@ -115,11 +96,8 @@ const maFeatures = {
     {
       type: "Feature",
       properties: {
-        entry_id: 106,
         url: "https://goo.gl/maps/ZthiZyT2CBVe4McW9",
         name: "Emilio's Cafe",
-        modified_date: "2021/01/03",
-        created_date: null,
         short_description: "Not just a cafe, a small gem for dining.",
         long_description:
           "Emilios is an solid choice in the foodie desert that is Manuel Antonio. The onsite bakery is good and the plates are way above average for the area.",
@@ -130,5 +108,69 @@ const maFeatures = {
         coordinates: [-84.15746, 9.40508],
       },
     },
+    {
+      type: "Feature",
+      properties: {
+        url:
+          "http://www.rainmakercostarica.org/index.php?option=com_content&task=view&id=16&Itemid=29",
+        name: "Rainmaker",
+        short_description: "A private conservation project in the mountains.",
+        long_description:
+          "When you've seen enough beach, this 1,500 acre reserve offers outstanding biodiversity and educational tours high in the mountains above Manuel Antonio. Tours run about 2 hours and are appropriate for anyone who's moderately fit.",
+        picture: "rainmaker.jpg",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-84.21504, 9.57698],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        url: "",
+        name: "Uvita Beach",
+        short_description: 'A unique "whaletail" beach',
+        long_description:
+          "Accessible at low tide, this unique beach offers a neighboring marine national park on a day trip that includes hipster Dominical, Nauyaca waterfalls and lunch at Punta Domenical. Check the tide charts before you go.",
+        picture: "uvita.jpg",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-83.76041, 9.14738],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        url: "http://nauyacawaterfallscostarica.com",
+        name: "Nauyaca Waterfalls",
+        short_description: "Lovely of falls with swimming and diving.",
+        long_description:
+          "Owned by a Costa Rican family that has maintained the land’s beauty without compromising its natural importance as part of the Rio Barucito. For individuals seeking the tranquility of nature as well as those looking for adventure!",
+        picture: "nauyaca.jpg",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-83.80821, 9.25589],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        url: "",
+        name: "Dominical",
+        short_description:
+          "A yoga, surfing and hipster haven just south of Quepos",
+        long_description:
+          "A low key town popular among singles and couples focusing on yoga and surfing. Check out the Fuego Brew Company for local ale.",
+        picture: "domenical.jpg",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-83.865, 9.25257],
+      },
+    },
   ],
 };
+
+// Rainmaker/@9.505728,-84.2227095,13z/data=!4m8!1m2!2m1!1srainmaker+manuel+antonio!3m4!1s0x8fa10bdb04027b8f:0x1645e3ce90e18b8c!8m2!3d9.5773362!4d-84.2142981
